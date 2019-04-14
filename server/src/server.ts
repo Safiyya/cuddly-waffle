aea74e9abb1511105b72c4052fc69ce9f9788813
Safiyya <safiyya.babio@gmail.com>
2019-04-14 19:54:00 +0300

    server returns colors for all divs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,40 +1,88 @@
 
 import express from "express";
-import * as request from "request-promise";
 import puppeteer from "puppeteer";
-
+import bodyParser from "body-parser";
 const server: express.Application = express();
 
-server.use(function(req, res, next) {
+server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+server.use(bodyParser.text({ type: 'text/html', limit: 100000000 }))
 
-server.get('/:url', function (req: express.Request, res: express.Response) {
+server.get('/get/:url', function (req: express.Request, res: express.Response) {
   let url = req.params.url;
-  console.log("get", url)
   puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url,{waitUntil: 'networkidle0'}).then(function() {
-      return page.content();
+    .launch()
+    .then(function (browser) {
+      return browser.newPage();
+    })
+    .then(function (page) {
+      return page.goto(url, { waitUntil: 'networkidle0' }).then(function () {
+        return page.content();
+      });
+    })
+    .then(function (html) {
+      res.send(html);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
     });
-  })
-  .then(function(html) {
-    // console.log(html)
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    
-    res.send(html);
-  })
-  .catch(function(err) {
-    res.status(500).send(err);
-    console.error(url, err)
-  });
+});
+
+/*
+public traversePromise(root:TreeNode, callback: ((n: TreeNode) => Promise<void>)): Array<Promise<void>> {
+    let queue: Array<Promise<void>> = [];
+
+    let recursion = (node: TreeNode) => {
+      if (node.children) {
+        node.children.forEach(function (child: TreeNode) {
+          queue.push(callback.apply(this, [child]));
+          recursion(child)
+        });
+      }
+    }
+
+    recursion(root);
+    return queue;
+  }
+  */
+
+server.post('/parse/', function (req: express.Request, res: express.Response) {
+  let html = req.body;
+
+  puppeteer
+    .launch()
+    .then(function (browser) {
+      return browser.newPage();
+    })
+    .then(function (page) {
+      return page.setContent(html).then(() => { return page });
+    })
+    .then(function (page) {
+     
+      return page.$$eval("div", elements => {
+
+        const getElement = (el: HTMLElement): { tag: string, class: string, color: string, backgroundColor: string } => {
+          let tag = el.tagName;
+          let color = getComputedStyle(el).color;
+          let backgroundColor = getComputedStyle(el).backgroundColor;
+          let classes = Array.from(el.classList.keys()).join(' ');
+
+          return { tag: tag, class: classes, color: color, backgroundColor: backgroundColor };
+        }
+        
+        return elements.map(d => getElement(d as HTMLElement))
+
+      })
+    })
+    .then(function (result) {
+      res.send(result)
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
 });
 
 
