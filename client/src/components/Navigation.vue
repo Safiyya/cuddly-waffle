c52bf1a49c0d21f839d9e07075235ad482f020e4
Safiyya <safiyya.babio@gmail.com>
2019-05-28 14:27:22 +0300

    styling

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,8 +1,8 @@
 
 <template>
   <header>
-    <nav class="my-3">
-      <h1 class="text-grey-darker flex items-end justify-center">
+    <nav class="my-3 flex flex-col items-start justify-center">
+      <h1 class="text-grey-darker flex items-end">
         <img
           class="w-6 h-6"
           src="@/assets/logo.svg"
@@ -11,18 +11,18 @@
         >
         <span class="mx-1 font-bold uppercase text-xl">CuddlyWaffle</span>
       </h1>
+      <h1 class="my-0">Stop wasting time with color pickers</h1>
+      <h2 class="my-0">Download the colors from any website. No color picker, no palette generator.</h2>
+
     </nav>
   </header>
 
 </template>
 <script lang="ts">
-
 import { Vue, Component, Prop, Provide, Watch } from "vue-property-decorator";
 
-@Component({
-  
-})
-export default class Navigation  extends Vue {}
+@Component({})
+export default class Navigation extends Vue {}
 </script>
 
 <style>
